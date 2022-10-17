101d0daca20e85bece5aba4872777ff03abe679f
anuragpatle <anurag.ap6@gmail.com>
2022-10-17 19:39:21 +0530

    Add fan ON code

--- RaspberryPiApp/SensorsMain.py
+++ RaspberryPiApp/SensorsMain.py
@@ -13,7 +13,9 @@ TEMPERATURE = 20.0
 HUMIDITY = 60
 PAYLOAD = '{{"temperature": {temperature}, "humidity": {humidity}, "moisture": {moisture}}}'
 
-soilMoistureSensorPin = 21
+SOIL_MOISTURE_PIN = 21 #GPIO 21 OR pin number 40
+FAN_PIN = 24
+SPRINKLER_PIN = 25
 moisture = "false" 
 
 async def main():
@@ -30,22 +32,28 @@ async def main():
         # Read data using pin GPIO17
         dhtDevice = dht11.DHT11(pin=17)
          
-        GPIO.setup(soilMoistureSensorPin, GPIO.IN) #Soil Moisture Sensor
+        GPIO.setup(SOIL_MOISTURE_PIN, GPIO.IN) #Soil Moisture Sensor
+        GPIO.setup(FAN_PIN, GPIO.OUT)
+        GPIO.setup(SPRINKLER_PIN, GPIO.OUT)
+
  
         print("Sending serivce started. Press Ctrl-C to exit")
         while True:
- 
+            time.sleep(1)
             try:
                 #DHT11
                 result = dhtDevice.read()
                 
                 #Soil Moisture
-                if GPIO.input(soilMoistureSensorPin):
+                if GPIO.input(SOIL_MOISTURE_PIN):
                     print ("Water NOT Detected!")
                     moisture = "false" 
+                    GPIO.output(FAN_PIN, GPIO.HIGH)
                 else:
                     print ("Water Detected!")
                     moisture = "true" 
+                    GPIO.output(FAN_PIN, GPIO.LOW)
+
 
                 print("result.is_valid(): ", result.is_valid())
 
