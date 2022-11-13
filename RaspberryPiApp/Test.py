043a8d12a7b359f66307711353b26b4ae8c992ae
anuragpatle <anurag.ap6@gmail.com>
2022-11-13 21:24:40 +0530

    Add voice

--- RaspberryPiApp/Test.py
+++ RaspberryPiApp/Test.py
@@ -22,5 +22,32 @@ import time
 # 	time.sleep(0.5)
 
 
-################################################## GUI Test ######################################################
+################################################## Speech Test ######################################################
 
+# Import the gTTS module for text
+# to speech conversion
+from gtts import gTTS
+
+# This module is imported so that we can
+# play the converted audio
+
+from playsound import playsound
+
+# It is a text value that we want to convert to audio
+text_val = 'Lights Turned OFF!'
+
+# Here are converting in English Language
+language = 'en'
+
+# Passing the text and language to the engine,
+# here we have assign slow=False. Which denotes
+# the module that the transformed audio should
+# have a high speed
+obj = gTTS(text=text_val, lang=language, slow=False)
+
+#Here we are saving the transformed audio in a mp3 file named
+# exam.mp3
+obj.save("./Files/lights_turned_on.mp3")
+
+# Play the exam.mp3 file
+playsound("./Files/lights_turned_off.mp3") # if this line gives you error then: pip install playsound==1.2.2
